7db9d5a1bc6c15d69fc875f5e65c430673095214
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2023-12-18 10:03:36 +0100

    preparing MVC structure

---
+++ App/connection/connect.php
@@ -0,0 +1,14 @@
+<?php
+require __DIR__.'/../../vendor/autoload.php';
+
+$dotenv  = Dotenv\Dotenv::createImmutable(__DIR__.'/../../');
+$dotenv->load();
+
+class CONNECT_SERVER{
+    public static function connect(){
+        return mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], $_ENV['DB_NAME']);
+    }
+}
+$connect = CONNECT_SERVER::connect();
+
+?>
