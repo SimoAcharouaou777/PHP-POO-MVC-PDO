4c7a8317cd5cccdc2009b7a4de7a2e1c3ac704e6
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2023-12-20 16:50:21 +0100

    working in autontification

--- App/Model/User.php
+++ App/Model/User.php
@@ -1,16 +1,41 @@
 <?php
-
+include_once(__DIR__.'/../Connection/connect.php');
 class User{
     private $id ;
     private $username;
-    private $full_name;
+    private $fullname;
     private $email;
     private $password;
-    private $role_id;
+    private $phone;
 
-    public static function Signup(){
-        $sql="INSERT INTO users (user_name ,full_name , email , password , role_id)
-        values (:user_name , :full_name , :email , :password , :role_id) ";
+    public function __construct(){
+        $this->id =$id;
+        $this->username = $username;
+        $this->fullname =$fullname;
+        $this->email = $email;
+        $this->password = $password;
+        $this->phone = $phone;
+    }
+
+    public static function creatUser($username , $fullname , $email, $password , $phone){
+        global $connect;
+        $sql="INSERT INTO users(user_name , full_name , email , password , phone )
+        values (:username , :fullname , :email , :password , :phone)";
+        $statement = $connect->prepare($sql);
+        $statement->bindParam(':username',$username , PDO::PARAM_STR);
+        $statement->bindParam(':fullname',$fullname, PDO::PARAM_STR);
+        $statement->bindParam(':email',$email, PDO::PARAM_STR);
+        $statement->bindParam(':password',$password, PDO::PARAM_STR);
+        $statement->bindParam(':phone',$phone, PDO::PARAM_STR);
+        $statement->execute();
+        $userid = $connect->lastInsertId();
+        $sql="INSERT INTO user_role(user_id) value(:user_id)";
+        $statement = $connect->prepare($sql);
+        $statement->bindParam(':user_id',$userid,PDO::PARAM_INT);
+        $statement->execute();
+    }
+    public static function getuser(){
+       
     }
 }
 
